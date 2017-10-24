0a721a3c92cc13de440f20c2e9cf6e66967b29ec
Sammy Wu <wu.sammy@gmail.com>
2017-10-24 02:06:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'tyrone';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'chiefkeef'
+}
+
+function attemptTwoFavoriteCustomers() {
+  const favoriteCustomer = 'miley'
+  let favoriteCustomer = 'charlie'
+  let favoriteCustomer = 'billie'
+}
